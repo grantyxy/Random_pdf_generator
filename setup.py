9bcb342548484a10e0f38c1dd73be7113f1a0934
Grant <grantyxy@outlook.com>
2021-09-29 04:14:20 -0400

    更改

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(name='random-pdf-generator-v2',
+                 version='0.2',
+                 description='A PDF generator with random content',
+                 url='https://github.com/grantyxy/Random-PDF-Generator',
+                 author='Xingyou Yu',
+                 author_email='grantyxy@outlook.com',
+                 license='GPL-3.0',
+                 install_requires=['fpdf'],
+                 zip_safe=False)
